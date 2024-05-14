172b833c616e1a5848f2154e9656c87ac99fe137
Aman kukna <aman.kukna.007@gmail.com>
2024-05-14 16:27:02 +0530

    responsive

--- src/addition page /header.js
+++ src/addition page /header.js
@@ -3,7 +3,7 @@ export default function Header(){
     const navigate = useNavigate();
 
     const project =()=>{
-        navigate("/#Projects")
+        navigate("/contact")
     }
     return(<>
 <div className="header flex">
@@ -12,9 +12,9 @@ export default function Header(){
 </div>
 <div className="flex hcenter">
     <h2 className="nav"><a href="/#Projects">Projects</a></h2>
-   <h2 className="nav none"> <a href="/#Technologies">Tecnologies</a></h2>
+   <h2 className="nav none tecno"> <a href="/#Technologies">Tecnologies</a></h2>
     <h2 className="nav none"><a href="/#about-me">About</a></h2>
-    <h2 className="nav"><a href="contact">Contact</a></h2>
+    <h2 className="nav" onClick={project}><a href="contact">Contact</a></h2>
     <h2 className="nav"><a href="get-in-touch">Get in Touch</a></h2>
 </div>
 
